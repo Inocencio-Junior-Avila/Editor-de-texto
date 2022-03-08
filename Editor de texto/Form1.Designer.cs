caf1c934805988d287218afff762187f2929ae91
Inocencio-Junior-Avila <20210836@itla.edu.do>
2022-03-07 22:38:56 -0600

    Agregar archivos de proyecto.

---
+++ Editor de texto/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace Editor_de_texto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.archivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardar = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrar = new System.Windows.Forms.ToolStripMenuItem();
+            this.edicion = new System.Windows.Forms.ToolStripMenuItem();
+            this.atras = new System.Windows.Forms.ToolStripMenuItem();
+            this.adelante = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.copiar = new System.Windows.Forms.ToolStripMenuItem();
+            this.cortar = new System.Windows.Forms.ToolStripMenuItem();
+            this.pegar = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.seleccionarTodo = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarTodo = new System.Windows.Forms.ToolStripMenuItem();
+            this.color = new System.Windows.Forms.ToolStripMenuItem();
+            this.formato = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivo,
+            this.edicion,
+            this.color,
+            this.formato});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 406);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // archivo
+            // 
+            this.archivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrir,
+            this.guardar,
+            this.cerrar});
+            this.archivo.Name = "archivo";
+            this.archivo.Size = new System.Drawing.Size(60, 20);
+            this.archivo.Text = "Archivo";
+            // 
+            // abrir
+            // 
+            this.abrir.Name = "abrir";
+            this.abrir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.abrir.Size = new System.Drawing.Size(180, 22);
+            this.abrir.Text = "Abrir";
+            this.abrir.Click += new System.EventHandler(this.abrir_Click);
+            // 
+            // guardar
+            // 
+            this.guardar.Name = "guardar";
+            this.guardar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.guardar.Size = new System.Drawing.Size(180, 22);
+            this.guardar.Text = "Guardar";
+            this.guardar.Click += new System.EventHandler(this.guardar_Click);
+            // 
+            // cerrar
+            // 
+            this.cerrar.Name = "cerrar";
+            this.cerrar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F4)));
+            this.cerrar.Size = new System.Drawing.Size(180, 22);
+            this.cerrar.Text = "Cerrar";
+            this.cerrar.Click += new System.EventHandler(this.cerrar_Click);
+            // 
+            // edicion
+            // 
+            this.edicion.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.atras,
+            this.adelante,
+            this.toolStripSeparator1,
+            this.copiar,
+            this.cortar,
+            this.pegar,
+            this.toolStripSeparator2,
+            this.seleccionarTodo,
+            this.eliminarTodo});
+            this.edicion.Name = "edicion";
+            this.edicion.Size = new System.Drawing.Size(58, 20);
+            this.edicion.Text = "Edicion";
+            // 
+            // atras
+            // 
+            this.atras.Name = "atras";
+            this.atras.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.atras.Size = new System.Drawing.Size(210, 22);
+            this.atras.Text = "Atras";
+            this.atras.Click += new System.EventHandler(this.atras_Click);
+            // 
+            // adelante
+            // 
+            this.adelante.Name = "adelante";
+            this.adelante.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Y)));
+            this.adelante.Size = new System.Drawing.Size(210, 22);
+            this.adelante.Text = "Adelante";
+            this.adelante.Click += new System.EventHandler(this.adelante_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(207, 6);
+            // 
+            // copiar
+            // 
+            this.copiar.Name = "copiar";
+            this.copiar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.copiar.Size = new System.Drawing.Size(210, 22);
+            this.copiar.Text = "Copiar";
+            this.copiar.Click += new System.EventHandler(this.copiar_Click);
+            // 
+            // cortar
+            // 
+            this.cortar.Name = "cortar";
+            this.cortar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.cortar.Size = new System.Drawing.Size(210, 22);
+            this.cortar.Text = "Cortar";
+            this.cortar.Click += new System.EventHandler(this.cortar_Click);
+            // 
+            // pegar
+            // 
+            this.pegar.Name = "pegar";
+            this.pegar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.pegar.Size = new System.Drawing.Size(210, 22);
+            this.pegar.Text = "Pegar";
+            this.pegar.Click += new System.EventHandler(this.pegar_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(207, 6);
+            // 
+            // seleccionarTodo
+            // 
+            this.seleccionarTodo.Name = "seleccionarTodo";
+            this.seleccionarTodo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.seleccionarTodo.Size = new System.Drawing.Size(210, 22);
+            this.seleccionarTodo.Text = "Seleccionar Todo";
+            this.seleccionarTodo.Click += new System.EventHandler(this.seleccionarTodo_Click);
+            // 
+            // eliminarTodo
+            // 
+            this.eliminarTodo.Name = "eliminarTodo";
+            this.eliminarTodo.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.X)));
+            this.eliminarTodo.Size = new System.Drawing.Size(210, 22);
+            this.eliminarTodo.Text = "Eliminar Todo";
+            this.eliminarTodo.Click += new System.EventHandler(this.eliminarTodo_Click);
+            // 
+            // color
+            // 
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(48, 20);
+            this.color.Text = "Color";
+            this.color.Click += new System.EventHandler(this.color_Click);
+            // 
+            // formato
+            // 
+            this.formato.Name = "formato";
+            this.formato.Size = new System.Drawing.Size(64, 20);
+            this.formato.Text = "Formato";
+            this.formato.Click += new System.EventHandler(this.formato_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Editor de Texto :)";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivo;
+        private System.Windows.Forms.ToolStripMenuItem abrir;
+        private System.Windows.Forms.ToolStripMenuItem guardar;
+        private System.Windows.Forms.ToolStripMenuItem cerrar;
+        private System.Windows.Forms.ToolStripMenuItem edicion;
+        private System.Windows.Forms.ToolStripMenuItem atras;
+        private System.Windows.Forms.ToolStripMenuItem adelante;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem copiar;
+        private System.Windows.Forms.ToolStripMenuItem cortar;
+        private System.Windows.Forms.ToolStripMenuItem pegar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem seleccionarTodo;
+        private System.Windows.Forms.ToolStripMenuItem eliminarTodo;
+        private System.Windows.Forms.ToolStripMenuItem color;
+        private System.Windows.Forms.ToolStripMenuItem formato;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Editor de texto/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Editor_de_texto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrir_Click(object sender, EventArgs e)
+        {
+            string r;
+            openFileDialog1.ShowDialog();
+            System.IO.StreamReader archivo = new System.IO.StreamReader(openFileDialog1.FileName);
+            r = archivo.ReadLine();
+            richTextBox1.Text = r.ToString();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "Sin Titulo.txt";
+            var guardar = saveFileDialog1.ShowDialog();
+            if (guardar == DialogResult.OK)
+            {
+                using (var guardar_archivo = new System.IO.StreamWriter(saveFileDialog1.FileName))
+                {
+                    guardar_archivo.WriteLine(richTextBox1.Text);
+                }
+            }
+        }
+
+        private void cerrar_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void atras_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void adelante_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void copiar_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void cortar_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void pegar_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void seleccionarTodo_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void eliminarTodo_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void color_Click(object sender, EventArgs e)
+        {
+            var cl = colorDialog1.ShowDialog();
+            if (cl == DialogResult.OK)
+            {
+                richTextBox1.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void formato_Click(object sender, EventArgs e)
+        {
+            var frmt = fontDialog1.ShowDialog();
+            if(frmt == DialogResult.OK)
+            {
+                richTextBox1.Font = fontDialog1.Font;
+            }
+        }
+    }
+}
